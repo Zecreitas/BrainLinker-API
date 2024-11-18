12983c7617e92a297577cffbcb198492115763f2
Zecreitas <schuh.gui@gmail.com>
2024-11-17 21:39:57 -0300

    Pronto

--- src/modal/db.js
+++ src/modal/db.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://Guilherme:310506gui@brainlinker.dn1zf.mongodb.net/?retryWrites=true&w=majority&appName=BrainLinker', {
-        });
-        console.log('MongoDB foi conectado');
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Conectando ao MongoDB com a URI:', process.env.MONGO_URI);
+
+    console.log('Conectado ao MongoDB Atlas com sucesso');
+  } catch (error) {
+    console.error('Erro ao conectar ao MongoDB:', error.message);
+    process.exit(1); 
+  }
 };
 
 module.exports = connectDB;
